dfdd1d57ae4b1ece2bc13611dee347ab717bd075
PresentLee <darkmagicsun@gmail.com>
2018-04-18 22:57:55 -0700

    modified, error happen at ls

--- minish.c
+++ minish.c
@@ -9,7 +9,6 @@
 
 
 #define BUFF_SIZE 100
-#define BUF_SIZE 100
 
 char file01[BUFF_SIZE];
 char file02[BUFF_SIZE];
@@ -19,14 +18,19 @@ void ls()
 	printf("start \n");
     DIR *dp;
     struct dirent *ep;
+	int fd;
+	off_t sz_file;
 	dp = opendir("./");
-
-    if(dp != NULL)
+   
+	if(dp != NULL)
 	{
 		printf("write \n");
 		while((ep = readdir(dp)) != NULL )
 		{
-       		puts (ep->d_name);
+		//	fd = open(ep->d_name,O_RDONLY);
+		//	sz_file =lseek(fd, 0, SEEK_END);
+			puts (ep->d_name);
+		//	printf("%s %ld",ep->d_name, sz_file);
 		}
 		closedir(dp);
 	}
@@ -45,35 +49,34 @@ return;
 
 int cp(char *argv, char *argv2)
 {
-	int argc;
 	int inputFd, outputFd, openFlags;
 	mode_t filePerms;
 	ssize_t numRead;
-	char buf[BUF_SIZE];
+	char buf[BUFF_SIZE];
 
-	if (argc != 3 || strcmp(argv[1], "--help") == 0)
-		usageErr("%s old-file new-file\n", argv[0]);
-		inputFd = open(argv[1], O_RDONLY);
+	inputFd = open(argv, O_RDONLY);
 	if (inputFd == -1)
-		errExit("opening file %s", argv[1]);
-		openFlags = O_CREAT | O_WRONLY | O_TRUNC;
-		filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; 
+		printf("opening file %s", argv);
 
-		outputFd = open(argv[2], openFlags, filePerms);
+	openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH; 
+
+		outputFd = open(argv2, openFlags, filePerms);
 	if (outputFd == -1)
-		errExit("opening file %s", argv[2]);
+		printf("opening file %s", argv2);
 
-	while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+	while ((numRead = read(inputFd, buf, BUFF_SIZE)) > 0)
+	{
 		if (write(outputFd, buf, numRead) != numRead)
-			fatal("couldn't write whole buffer");
+			printf("couldn't write whole buffer");
 		if (numRead == -1)
-			errExit("read");
+			printf("read");
 		if (close(inputFd) == -1)
-			errExit("close input");
+			printf("close input");
 		if (close(outputFd) == -1)
-			errExit("close output");
-								    
-		exit(EXIT_SUCCESS);
+			printf("close output");
+	}							    
+		return 0;
 
 }
 
@@ -97,34 +100,26 @@ int main(int argc, char **argv)
 	    memset(file02, 0, sizeof(file02));
 		
 		scanf("%s", mode);
-		/*
-		*ptr = strtok(mode," ");
-		ptr = strtok(NULL, " ");
-		ptr = strtok(NULL, " ");
-		strcpy(file01,ptr);
-		ptr = strtok(NULL," ");
-		strcpy(file02 ,ptr);
-*/
-        	if(mode[0] == 'l' && mode[1] =='s' )
-        	{
-				ls();
-        	}
-        	if(mode[0] == 'c'&& mode[1]=='p')
-        	{
-				scanf("%s %s", file01, file02);
-            	cp(file01,file02);
-        	}
-			if(mode[0] == 'r'&& mode[1]=='m')
-        	{
-				scanf("%s", file01);
-            	rm(file01);
-        	}
-			if(strcmp(mode, "exit")==0)
-			{
-				exit(1);
-
-			}
-		
+        	
+		if(mode[0] == 'l' && mode[1] =='s' )
+        {
+			ls();
+        }
+        if(mode[0] == 'c'&& mode[1]=='p')
+        {
+			scanf("%s %s", file01, file02);
+           	cp(file01,file02);
+        }
+		if(mode[0] == 'r'&& mode[1]=='m')
+        {
+			scanf("%s", file01);
+           	rm(file01);
+        }
+		if(strcmp(mode, "exit")==0)
+		{
+			exit(1);
+		}
+	
     }
 
 return 0;
